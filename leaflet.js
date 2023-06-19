80feb5f2d987b441ffda2ce117ba9aa49dcc1d7c
Your Name <trenchfam.helorocino@gmail.com>
2023-06-20 00:41:36 +0800

    marker

--- leaflet.js
+++ leaflet.js
@@ -1,5 +1,7 @@
 var map = L.map('map').setView([4.19303, 101.99707], 8);
 
+var marker = L.marker([4.19303, 101.99707]).addTo(map);
+
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
