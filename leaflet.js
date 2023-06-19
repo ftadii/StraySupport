aa8c8cbd22b57bcfaaec51e2f6be4ee44ccf8543
Your Name <trenchfam.helorocino@gmail.com>
2023-06-20 00:40:07 +0800

    zoom

--- leaflet.js
+++ leaflet.js
@@ -1,4 +1,4 @@
-var map = L.map('map').setView([4.19303, 101.99707], 15);
+var map = L.map('map').setView([4.19303, 101.99707], 20);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
