cf43baccec8b5e640c97bb3756a7bd7a2f49edbe
Your Name <trenchfam.helorocino@gmail.com>
2023-06-20 00:46:00 +0800

    popup

--- leaflet.js
+++ leaflet.js
@@ -1,7 +1,7 @@
 var map = L.map('map').setView([4.19303, 101.99707], 8);
 
 var marker = L.marker([4.19303, 101.99707]).addTo(map);
-marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
+marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup().openOn(map);;
 
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
