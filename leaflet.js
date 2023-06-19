926da712dce43c2f5de5b0fdd77ecf6808998205
Your Name <trenchfam.helorocino@gmail.com>
2023-06-19 21:54:53 +0800

    pls? meow

--- leaflet.js
+++ leaflet.js
@@ -1,3 +1,6 @@
-<script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"
-     integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo="
-     crossorigin=""></script>
+var map = L.map('map').setView([51.505, -0.09], 13);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
