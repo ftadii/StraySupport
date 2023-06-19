a134fc9805dc49f5566554cded5f8877e26709d7
Your Name <trenchfam.helorocino@gmail.com>
2023-06-19 22:25:51 +0800

    map location default

--- leaflet.js
+++ leaflet.js
@@ -1,4 +1,4 @@
-var map = L.map('map').setView([51.505, -0.09], 13);
+var map = L.map('map').setView([4.19303, 101.99707], 13);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
