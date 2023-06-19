e75b5b8efbcc3bc2b9aa592848c21fc55ce64a47
Your Name <trenchfam.helorocino@gmail.com>
2023-06-20 00:43:11 +0800

    tah tuk apa

--- leaflet.js
+++ leaflet.js
@@ -3,6 +3,10 @@ var map = L.map('map').setView([4.19303, 101.99707], 8);
 var marker = L.marker([4.19303, 101.99707]).addTo(map);
 marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
 
+var popup = L.popup()
+    .setLatLng([51.513, -0.09])
+    .setContent("I am a standalone popup.")
+    .openOn(map);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
