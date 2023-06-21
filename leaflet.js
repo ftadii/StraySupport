28bc2b92908c7b7b2bd95d4142ad8238a1b082a9
Adi <trenchfam.helorocino@gmail.com>
2023-06-21 19:18:49 +0800

    test

--- leaflet.js
+++ leaflet.js
@@ -1,7 +1,7 @@
 var map = L.map('map').setView([4.19303, 101.99707], 8);
 
-var marker = L.marker([4.19303, 101.99707]).addTo(map);
-marker.bindPopup("Popup Test!");
+//var marker = L.marker([4.19303, 101.99707]).addTo(map);
+//marker.bindPopup("Popup Test!");
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
